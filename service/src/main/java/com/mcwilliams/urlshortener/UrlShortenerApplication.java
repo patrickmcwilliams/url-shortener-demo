5871d8a6956f2bf780c8ad3798d22488f440c646
Patrick McWilliams <gavins.dad@gmail.com>
2019-04-15 06:39:43 -0700

    added dockerfile for service

--- service/src/main/java/com/mcwilliams/urlshortener/UrlShortenerApplication.java
+++ service/src/main/java/com/mcwilliams/urlshortener/UrlShortenerApplication.java
@@ -2,10 +2,11 @@ package com.mcwilliams.urlshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class UrlShortenerApplication {
+public class UrlShortenerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortenerApplication.class, args);
